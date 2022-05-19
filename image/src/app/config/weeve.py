e65f8153468ba8cde351f3a9bd8d249302cb9a75
Ghassen <ghassen@weeves-Air-3.local>
2022-05-19 12:57:40 +0200

    WD-374-modularize-the-serial-receiver

--- image/src/app/config/weeve.py
+++ image/src/app/config/weeve.py
@@ -4,10 +4,10 @@ All constants specific to weeve
 from app.utils.env import env
 
 WEEVE = {
-    "MODULE_NAME": env("MODULE_NAME", "python-ingress-boilerplate"),
+    "MODULE_NAME": env("MODULE_NAME", "serial ingress"),
     "MODULE_TYPE": env("MODULE_TYPE", "INGRESS"),
-    "Port":env("Port","/dev/cu.usbserial-1130"),
-    "Baud_rate":env("Baud_rate","115200"),
+    "PORT":env("Port","/dev/cu.usbserial-130"),
+    "BAUD_RATE":env("Baud Rate","115200"),
     "EGRESS_URL": env("EGRESS_URL", "https://hookb.in/oXkwLNJEEjHYX7mgwq6g")
 
 }
