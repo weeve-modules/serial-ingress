c8ec8cfd3f99abecd866414b1ea3ca516c9d6cbb
GhasBarbouchi <ghassen.barbouchi@weeve.network>
2022-05-31 15:08:48 +0200

    WD-374-modularize-the-serial-receiver few updates

--- image/src/app/config/weeve.py
+++ image/src/app/config/weeve.py
@@ -5,5 +5,6 @@ from app.utils.env import env
 
 WEEVE = {
     "MODULE_NAME": env("MODULE_NAME", "serial-ingress"),
-    "MODULE_TYPE": env("MODULE_TYPE", "INGRESS")
+    "MODULE_TYPE": env("MODULE_TYPE", "INGRESS"),
+    "EGRESS_URL": env("EGRESS_URL", "http://0.0.0.0:80")
 }
