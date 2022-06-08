b4f01aa6427447285a465920a34e5ee38ed0c74d
GhasBarbouchi <ghassen.barbouchi@weeve.network>
2022-06-08 15:08:48 +0200

    Code style and new variables environment updates:
    1.Remove the Indentation
    2. Import serial in application to identify parity and fix the type of others variables in Module.yaml
    3.fix README.md vocabulary

--- image/src/app/config/application.py
+++ image/src/app/config/application.py
@@ -5,11 +5,12 @@ import serial
 from app.utils.env import env
 
 APPLICATION = {
-    "PORT": env("PORT","/dev/ttyUSB0"),
-    "BAUD_RATE": env("BAUD_RATE","115200"),
-     "DATA_BITS": env("DATA_BITS",8),
-     "PARITY": env("PARITY",serial.PARITY_NONE),
-      "STOP_BITS": env("STOP_BITS",1)
+"PORT": env("PORT","/dev/ttyUSB0"),
+"BAUD_RATE": env("BAUD_RATE","115200"),
+"DATA_BITS": env("DATA_BITS",8),
+"PARITY": env("PARITY",serial.PARITY_NONE),
+"STOP_BITS": env("STOP_BITS",1)
       
 }
+
 
