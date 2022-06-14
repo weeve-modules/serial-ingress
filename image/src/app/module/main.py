64f002c3205da0a341e437274ac450f5f7d90b04
GhasBarbouchi <ghassen.barbouchi@weeve.network>
2022-06-14 12:56:13 +0200

    Improve code style
    .use more meaningful name for functions
    .use named constant for stop bits and data bits

--- image/src/app/module/main.py
+++ image/src/app/module/main.py
@@ -6,6 +6,14 @@ import serial
 import json
 from app.weeve.egress import send_data
 from app.config import APPLICATION
+
+def parity_translation(sParity=APPLICATION['PARITY']):
+    switcher = {
+       "None" : "N",
+       "Even" : "E",
+        "Odd" : "O"
+    }
+    return switcher.get(sParity, lambda: "Invalid parity")
 def module_main():
     """implement module logic here
 
@@ -17,16 +25,15 @@ def module_main():
     """
     try:
         #  open the serial port and get the serial port object
-        ser = serial.Serial(APPLICATION['PORT'], APPLICATION['BAUD_RATE'], timeout=1,bytesize=APPLICATION['DATA_BITS'],parity=APPLICATION['PARITY'], stopbits=APPLICATION['STOP_BITS'])
-        while True: 
-          #  read json payload
-          ser.reset_input_buffer()
-          data = ser.readline().decode("ISO-8859-1")
-          dict_json = json.loads(data)
-          sent=send_data(dict_json)
-          if sent:
-            print("SUCCESS"),print("failed")
+        ser = serial.Serial(APPLICATION['PORT'], APPLICATION['BAUD_RATE'], timeout=1,bytesize=APPLICATION['DATA_BITS'],parity=parity_translation(), stopbits=APPLICATION['STOP_BITS'])
+        while True:
+        #  read json payload
+         ser.reset_input_buffer()
+         data = ser.readline().decode("ISO-8859-1")
+         dict_json = json.loads(data)
+         sent=send_data(dict_json)
+         if sent:
+          print("SUCCESS"),print("failed")
           print(dict_json)
     except json.JSONDecodeError as e:
       return None, f"Unable to perform the module logic: {e}"
-      
