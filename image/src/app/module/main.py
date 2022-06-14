3cf6237af5078ca0bc5c674476925d203490e34f
GhasBarbouchi <ghassen.barbouchi@weeve.network>
2022-06-14 16:53:31 +0200

    Fix code style and throw error in parity_translation function

--- image/src/app/module/main.py
+++ image/src/app/module/main.py
@@ -6,14 +6,16 @@ import serial
 import json
 from app.weeve.egress import send_data
 from app.config import APPLICATION
-
-def parity_translation(sParity=APPLICATION['PARITY']):
+def parity_translation(serial_parity=APPLICATION['PARITY']):
     switcher = {
-       "None" : "N",
-       "Even" : "E",
-        "Odd" : "O"
+       "None" : serial.PARITY_NONE,
+       "Even" : serial.PARITY_EVEN,
+       "Odd"  : serial.PARITY_ODD
     }
-    return switcher.get(sParity, lambda: "Invalid parity")
+    try:
+        return switcher.get(serial_parity)
+    except KeyError:
+        print("Invalidate parity")
 def module_main():
     """implement module logic here
 
@@ -27,13 +29,15 @@ def module_main():
         #  open the serial port and get the serial port object
         ser = serial.Serial(APPLICATION['PORT'], APPLICATION['BAUD_RATE'], timeout=1,bytesize=APPLICATION['DATA_BITS'],parity=parity_translation(), stopbits=APPLICATION['STOP_BITS'])
         while True:
-        #  read json payload
-         ser.reset_input_buffer()
-         data = ser.readline().decode("ISO-8859-1")
-         dict_json = json.loads(data)
-         sent=send_data(dict_json)
-         if sent:
-          print("SUCCESS"),print("failed")
-          print(dict_json)
-    except json.JSONDecodeError as e:
-      return None, f"Unable to perform the module logic: {e}"
+            #  read json payload
+            ser.reset_input_buffer()
+            data = ser.readline().decode("ISO-8859-1")
+            dict_json = json.loads(data)
+            sent=send_data(dict_json)
+            if sent:
+                print("The data successfully sent")
+                print(dict_json)
+            else :
+                print("Failed to send the data")
+    except json.JSONDecodeError as err:
+        return None, f"Unable to perform the module logic: {err}"
