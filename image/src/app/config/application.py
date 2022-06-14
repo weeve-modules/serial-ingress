7f3ae98e2dc15a74c112b103c24459210ad966d7
GhasBarbouchi <104772068+GhasBarbouchi@users.noreply.github.com>
2022-06-14 14:13:37 +0200

    Update application.py

--- image/src/app/config/application.py
+++ image/src/app/config/application.py
@@ -5,13 +5,7 @@ from app.utils.env import env
 APPLICATION = {
     "PORT":      env("PORT","/dev/ttyUSB0"),
     "BAUD_RATE": env("BAUD_RATE","115200"),
-<<<<<<< HEAD
     "DATA_BITS": env("DATA_BITS",serial.EIGHTBITS),
     "PARITY":    env("PARITY","None"),
     "STOP_BITS": env("STOP_BITS",serial.STOPBITS_ONE)
-=======
-    "DATA_BITS": env("DATA_BITS","EIGHTBITS"),
-    "PARITY":    env("PARITY",'None'),
-    "STOP_BITS": env("STOP_BITS","STOPBITS_ONE")
->>>>>>> c0f28a2ded11f8720b30dd3dc799fb30ba26ae34
 }
